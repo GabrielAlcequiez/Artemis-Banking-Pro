171a91b16ca228d86a08cda9dd2ef96e74aaa47d
Gabriel Alcequiez <gxbbab@gmail.com>
2026-08-03 21:49:56 -0400

    feat: implement global exception handling and correlation ID middleware, add error view model and related views

--- ABP.WebApi/Program.cs
+++ ABP.WebApi/Program.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
 using ABP.Infrastructure.Identity;
+using ABP.Shared.Middleware;
+using ABP.WebApi.Handler;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +23,8 @@ builder.Host.UseSerilog();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -31,6 +36,20 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCorrelationId();
+app.UseExceptionHandler();
+app.UseRouting();
+app.UseSerilogRequestLogging(o =>
+{
+    o.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("CorrelationId", httpContext.TraceIdentifier);
+        diagnosticContext.Set("Endpoint", httpContext.GetEndpoint()?.DisplayName);
+        diagnosticContext.Set("UserName", httpContext.User.Identity?.Name);
+        diagnosticContext.Set("Role", httpContext.User.FindFirst(ClaimTypes.Role)?.Value);
+    };
+});
 
 app.UseHttpsRedirection();
 
--- ABP.WebApp/Program.cs
+++ ABP.WebApp/Program.cs
@@ -1,6 +1,9 @@
+using System.Security.Claims;
 using ABP.Infrastructure.Identity;
 using ABP.Infrastructure.Persistence;
 using ABP.Shared;
+using ABP.Shared.Middleware;
+using ABP.WebApp.Handler;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,23 +23,36 @@ builder.Services.AddInfrastructureIdentityServices(builder.Configuration);
 builder.Services.AddInfrastructurePersistence(builder.Configuration);
 builder.Services.AddSharedServices(builder.Configuration);
 builder.Services.AddControllersWithViews();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
 // await app.Services.RunSeedsAsync();
 
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapDefaultControllerRoute();
-
+app.UseCorrelationId();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
+app.UseExceptionHandler();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSerilogRequestLogging(o =>
+{
+    o.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("CorrelationId", httpContext.TraceIdentifier);
+        diagnosticContext.Set("Endpoint", httpContext.GetEndpoint()?.DisplayName);
+        diagnosticContext.Set("UserName", httpContext.User.Identity?.Name);
+        diagnosticContext.Set("Role", httpContext.User.FindFirst(ClaimTypes.Role)?.Value);
+    };
+});
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
 
 try
 {
