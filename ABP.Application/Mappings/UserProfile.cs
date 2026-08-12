f496eca685f8d20bbd736d2f85c68c61db6df1b3
jrc03 <joserdlc2727@gmail.com>
2026-08-11 20:30:22 -0400

    fix: complete user mapping profiles

--- ABP.Application/Mappings/UserProfile.cs
+++ ABP.Application/Mappings/UserProfile.cs
@@ -14,23 +14,48 @@ namespace ABP.Application.Mappings
             // 1. CreateUserDto -> User
             CreateMap<CreateUserDto, User>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName))
-                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => Enum.Parse<Roles>(src.Role)));
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => Enum.Parse<Roles>(src.Role)))
+                .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+                .ForMember(dest => dest.SavingsAccounts, opt => opt.Ignore())
+                .ForMember(dest => dest.CreditCards, opt => opt.Ignore())
+                .ForMember(dest => dest.Loans, opt => opt.Ignore())
+                .ForMember(dest => dest.Beneficiaries, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAtUtc, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedByUserId, opt => opt.Ignore())
+                .ForMember(dest => dest.LastModifiedAtUtc, opt => opt.Ignore())
+                .ForMember(dest => dest.LastModifiedByUserId, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
 
             // 2. EditUserDto -> User
             CreateMap<EditUserDto, User>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName));
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.Role, opt => opt.Ignore())
+                .ForMember(dest => dest.IsActive, opt => opt.Ignore())
+                .ForMember(dest => dest.CommerceId, opt => opt.Ignore())
+                .ForMember(dest => dest.SavingsAccounts, opt => opt.Ignore())
+                .ForMember(dest => dest.CreditCards, opt => opt.Ignore())
+                .ForMember(dest => dest.Loans, opt => opt.Ignore())
+                .ForMember(dest => dest.Beneficiaries, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAtUtc, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedByUserId, opt => opt.Ignore())
+                .ForMember(dest => dest.LastModifiedAtUtc, opt => opt.Ignore())
+                .ForMember(dest => dest.LastModifiedByUserId, opt => opt.Ignore())
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
 
             // 3. User -> GetUserDto
             CreateMap<User, GetUserDto>()
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()))
+                .ForMember(dest => dest.CommerceName, opt => opt.Ignore());
 
             // 4. User -> LoginResponseDto
             CreateMap<User, LoginResponseDto>()
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserName))
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.IsVerified, opt => opt.MapFrom(src => src.IsActive))
-                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => new List<string> { src.Role.ToString() }));
+                .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => new List<string> { src.Role.ToString() }))
+                .ForMember(dest => dest.HasError, opt => opt.Ignore())
+                .ForMember(dest => dest.Error, opt => opt.Ignore());
 
         }
     }
